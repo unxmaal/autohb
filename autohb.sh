36f30abffe65e2692b118f019b22a69e9b23d00b
Eric Dodd <eric.e.dodd@gmail.com>
2015-05-16 21:29:04 -0400

    user-provided titles

--- autohb.sh
+++ autohb.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 _dir=$1
-_name=$(basename "$_dir")
+_name=$2
+
 _outdir="/Users/eric/Desktop"
 
 autohb(){
+    if [[ -z "$_name" ]] ; then
+        _name=$(basename "$_dir")
+    fi
+
     /Applications/HandBrakeCLI -i "$_dir" -o "$_outdir/$_name".m4v --preset="Normal"
     _retval=$?
 
@@ -18,7 +23,7 @@ dvdeject(){
 
 main(){
    if [[ -z "${_dir}" ]] ; then
-       echo "Usage: ./autohb.sh <path to dvd>"
+       echo "Usage: ./autohb.sh <path to dvd> (<alternative name>)"
    else
        autohb
           if [[ $? -ne 0 ]] ; then
